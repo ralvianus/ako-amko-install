bce832ed707a30ba17ee32ddbbc883c6a6601886
Robert <robert@alvianus.net>
2021-07-02 14:44:11 +0800

    update..

--- ako-install.sh
+++ ako-install.sh
@@ -9,7 +9,7 @@ mkdir ~/helm-install
 cd ~/helm-install
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
-./get_helm.sh -v 3.0.3
+./get_helm.sh -v v3.0.3
 echo
 helm version
 echo
